e0c6d8d1dd0027499cc4bee97d23bbcf57444e70
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2022-01-31 13:23:49 +0000

    killing the cat

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -8,7 +8,7 @@ public class Main
         processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
 
-    public void cat()
+    public static void cat()
     {
         System.out.println("Hello world");
 
@@ -32,8 +32,8 @@ public class Main
     }
     public static void main(String[] args)
     {
-        helloProcessing();
-
+        cat();
+        //helloProcessing();
         // Tara Misty
         // Tara Tara
         
