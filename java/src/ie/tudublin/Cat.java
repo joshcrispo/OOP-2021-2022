01324c37669c02fac9dea3d49b274087da82c252
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2022-01-31 13:51:16 +0000

    Hello

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -4,33 +4,29 @@ public class Cat extends Animal
 {
     private int numLives;
 
-    public void kill()
+    public int getNumLives()
     {
-        if (numLives > 0)
-        {
-            System.out.println("Ouch!");
-            numLives --;
-        }
-        else
-        {
-            System.out.println("Im dead");
-        }
-    }
-
-    
-    public int getNumLives() {
         return numLives;
     }
-
-
-    public void setNumLives(int numLives) {
-        this.numLives = numLives;
-    }
-
-
+    
     public Cat(String name)
     {
         super(name);
-        numLives = 9;
+        this.numLives = 9;
+    }
+
+    public void kill()
+    {
+        numLives = getNumLives();
+
+        if (numLives > 0)
+        {
+            numLives -= 1;
+            System.out.println("Ouch!");
+        }
+        else
+        {
+            System.out.println("Dead");
+        }
     }
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -8,7 +8,7 @@ public class Main
         processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
 
-    public static void cat()
+    public void cat()
     {
         System.out.println("Hello world");
 
@@ -32,8 +32,8 @@ public class Main
     }
     public static void main(String[] args)
     {
-        cat();
-        //helloProcessing();
+        helloProcessing();
+
         // Tara Misty
         // Tara Tara
         
