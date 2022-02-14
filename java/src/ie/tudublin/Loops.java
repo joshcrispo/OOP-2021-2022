7988d273f266082aebb8ce7c58e7f446bd9a84a5
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2022-02-14 11:47:22 +0000

    updated loops w/ maps

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -34,6 +34,15 @@ public class Loops extends PApplet
 		switch(mode)	
 		{
 			case 0:
+				background(0);
+				int bars = (int) (mouseX / 20.0f);
+				float w = width / (float)bars;
+				for(int i = 0 ; i < bars; i++)
+				{
+					noStroke();
+					fill(map(i, 0, bars, 0, 255), 255, 255);
+					rect(map(i, 0, bars, 0, 500), 0, w, height);
+				}
 				break;
 			case 1:
 				break;
