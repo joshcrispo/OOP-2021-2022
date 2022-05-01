fd68d0dfdede5a6fe5a39befad5c7ad99add041b
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2022-05-01 17:31:29 +0100

    practicing stuff

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -45,6 +45,22 @@ public class Loops extends PApplet
 				}
 				break;
 			case 1:
+				background(0);
+				beginShape();
+				for(int i=0;i<360;i+=mouseX)
+				{
+					noFill();
+					float sides1 = radians(i);
+					float x = width/2;
+					float y = height/2;
+					float r = 150;
+
+					float centerX = x + sin(sides1) * r;
+					float centerY = y + cos(sides1) * r;
+					stroke(255);
+					line(x,y,centerX,centerY);
+				}
+				endShape();
 				break;
 		}
 	}
